fe729796e648528ab626e7e0812d48acdfb6cbbf
Gabriel <gabrieljimenez@ugr.es>
2025-06-19 07:46:14 +0200

    Balance light

--- train_scripts/execute_federated_fedprox.sh
+++ train_scripts/execute_federated_fedprox.sh
@@ -17,13 +17,13 @@ sleep "$(( JOB_ID % MODULO_N * MODULO_N ))"
 
 PORT=$(( JOB_ID + 8080 ))
 
-CLIENT_OPTS="xavier:${PORT} --wandb-project='${W_P}' --wandb-group='${GID}' --fedprox-pmu=${FEDPROX_PMU}"
+CLIENT_OPTS="xavier:${PORT} --wandb-project='${W_P}' --wandb-group='${GID}' --fedprox-pmu=${FEDPROX_PMU} --light"
 
 # shellcheck disable=SC2086
 readarray -t TRAIN_PATHS_OPT < <(ls -S ${DATA_PATH}/train/*csv)
 # shellcheck disable=SC2086
 readarray -t TEST_PATHS_OPT < <(ls -S ${DATA_PATH}/test/*csv)
-CLIENTS=('almogrote' 'almogrote' 'almogrote' 'almogrote' 'almogrote' 'gofio' 'gofio' 'gofio' 'gofio' 'platano' 'platano' 'platano' 'platano' 'citic' 'citic' 'citic' 'citic')
+CLIENTS=('almogrote' 'almogrote' 'almogrote' 'almogrote' 'almogrote' 'gofio' 'gofio' 'gofio' 'gofio' 'platano' 'platano' 'platano' 'platano' 'citic' 'citic' 'citic')
 
 exec_client() {
   echo "Sending commands to ${CLIENT_NAME}"
